c9a286189bd101d827cee64fef6790442d9d9dbe
commandstring <bmxmikey2@gmail.com>
2023-01-29 07:23:11 -0500

    Fixed SCSS compiler

--- scss/compile.php
+++ scss/compile.php
@@ -7,8 +7,8 @@ require_once "../vendor/autoload.php";
 
 $currentDirectory = realpath("./");
 $outputDirectory = realpath("../public/assets/css");
-$files = FileSystemUtils::getAllFilesWithExtensions("./", ["scss"], true);
-$directories = FileSystemUtils::getAllSubDirectories("./", true);
+$files = FileSystemUtils::getAllFiles(__DIR__, true);
+$directories = FileSystemUtils::getAllSubDirectories(__DIR__, true);
 $compiler = new Compiler();
 
 foreach ($directories as $directory) {
@@ -22,6 +22,10 @@ foreach ($directories as $directory) {
 }
 
 foreach ($files as $file) {
+    if (!str_ends_with($file, "scss")) {
+        continue;
+    }
+
     $scss_contents = file_get_contents($file);
     $compiled_css = $compiler->compileString($scss_contents)->getCss();
     $outputPath = str_replace(".scss", ".css", str_replace($currentDirectory, $outputDirectory, $file));
