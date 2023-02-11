c93609d1f1a9f67339e96bb66f21e77d798e1a00
commandstring <bmxmikey2@gmail.com>
2023-02-11 12:57:25 -0500

    Added getDriver method

--- common/Env.php
+++ common/Env.php
@@ -2,11 +2,17 @@
 
 namespace Common;
 
+use CommandString\Pdo\Driver;
 use eftec\bladeone\BladeOne;
 
 class Env extends \CommandString\Env\Env {
     public static function blade(): BladeOne
     {
         return clone Env::get()->blade;
+    }
+
+    public static function getDriver(): ?Driver
+    {
+        return self::get()->driver ?? null;
     }
 }
